1154512da8c2a072cf419983dbc1682bbeec5f6d
Thomas Johansen <thomasjo@gmail.com>
2016-11-19 13:01:57 +0100

    Improve and simplify vector_add example

--- example/vector_add/vector_add.cu
+++ example/vector_add/vector_add.cu
@@ -1,12 +1,11 @@
-#include "vector_add.cuh"
-
 #include <iostream>
+#include <random>
 #include <vector>
 
 #include <cudalicious/core.hpp>
 
-__global__
-void add_kernel(float* c, const float* a, const float* b, const size_t n)
+template<typename T>
+__global__ void add_kernel(T* c, const T* a, const T* b, const size_t n)
 {
   int idx = blockDim.x * blockIdx.x + threadIdx.x;
   if (idx < n) {
@@ -14,23 +13,35 @@ void add_kernel(float* c, const float* a, const float* b, const size_t n)
   }
 }
 
-std::vector<float> vector_add(std::vector<float> a, std::vector<float> b)
+template<typename T>
+std::vector<T> vector_add(const std::vector<T>& a, const std::vector<T>& b)
 {
-  auto n = a.size();
-  auto d_a = cuda::copy_to_device(a);
-  auto d_b = cuda::copy_to_device(b);
-  auto d_c = cuda::allocate<float>(n);
+  const auto n = a.size();
+  const auto d_a = cuda::copy_to_device(a);
+  const auto d_b = cuda::copy_to_device(b);
+  auto d_c = cuda::allocate<T>(n);
 
-  auto blocks = dim3(400);
-  auto threads = dim3(1024, 1);
+  auto kernel_start = cuda::event_create();
+  auto kernel_stop = cuda::event_create();
 
-  std::cout << "Launching kernel..." << std::endl;
+  const auto blocks = dim3(400);
+  const auto threads = dim3(1024, 1);
+
+  std::cout << "Launching kernel... " << std::flush;
+
+  cuda::event_record(kernel_start);
   add_kernel<<<blocks, threads>>>(d_c, d_a, d_b, n);
-  cuda::peek_at_last_error();
-  cuda::device_sync();
+  cuda::event_record(kernel_stop);
+  cuda::event_sync(kernel_stop);
 
-  std::vector<float> c(n);
+  auto msec_total = cuda::event_elapsed_time(kernel_start, kernel_stop);
+  std::cout << "finished in " << msec_total * 1000 << "ns\n";
+
+  std::vector<T> c(n);
   cuda::copy_to_host(c, d_c);
+
+  cuda::event_destroy(kernel_start);
+  cuda::event_destroy(kernel_stop);
 
   cuda::free(d_a);
   cuda::free(d_b);
@@ -39,4 +50,17 @@ std::vector<float> vector_add(std::vector<float> a, std::vector<float> b)
   cuda::device_reset();
 
   return c;
+}
+
+int main()
+{
+  constexpr auto n = 400000ul;
+  constexpr auto init_value = 1.0f;
+
+  std::vector<float> a(n, init_value);
+  std::vector<float> b(n);
+  std::iota(b.begin(), b.end(), init_value);
+
+  const auto result = vector_add(a, b);
+  std::cout << result[n - 1] << "\n";
 }
--- example/vector_add/vector_add.cuh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <vector>
-
-std::vector<float> vector_add(std::vector<float> a, std::vector<float> b);
